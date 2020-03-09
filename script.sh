8f3167e4f94ae47467189314eaeb15490ba27fbc
raw-ram <61500192+raw-ram@users.noreply.github.com>
2020-03-09 08:49:34 +0100

    Update script.sh
    some changes

--- script.sh
+++ script.sh
@@ -7,32 +7,9 @@ echo "smart_script" >> ~/.bashrc
 cp ./bash_aliases ~/.bash_aliases
 echo "[*] History and script in ~/.bashrc set."
 
-cp -v ./tmux.conf ~/.tmux.conf
-echo "[*] Tmux conf copied"
 mkdir -p ~/.config/terminator/
 cp -v ./terminator.rc ~/.config/terminator/config
 echo "[*] Terminator settings restored"
-
-gsettings set org.gnome.mutter dynamic-workspaces false
-gsettings set org.gnome.mutter workspaces-only-on-primary false
-
-gsettings set org.gnome.desktop.wm.preferences num-workspaces 5
-
-echo "5 workspaces set, on both screens."
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Alt>1']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Alt>2']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Alt>3']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Alt>4']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-last "['<Alt>5']"
-
-
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Primary>1']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Primary>2']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Primary>3']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Primary>4']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-last "['<Primary>5']"
-
-echo "[*] Shortcuts alt and ctrl set."
 
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type "'nothing'"
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type "'nothing'"
@@ -43,5 +20,8 @@ echo "[*] Tap to click on touchpad set."
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed "false"
 echo "[*] Autohide for dock enabled."
 
-apt update && apt -y install terminator vim tmux glances git
+apt update
+apt -y install terminator vim
+apt -y install glances #monitoring tool
+apt -y install brasero #4 burning cds
 echo "[*] Stuff installed"
