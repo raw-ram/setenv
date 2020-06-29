67208a72f86c7bd71972860c465d6d05e81cd93a
raw-ram <61500192+raw-ram@users.noreply.github.com>
2020-06-29 14:13:10 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,10 +22,10 @@ apt -y install terminator vim
 apt -y install glances #monitoring tool
 apt -y install brasero #4 burning cds
 
-git clone https://github.com/danielmiessler/SecLists.git /opt/
-git clone https://github.com/SecureAuthCorp/impacket.git /opt/
-git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/
-wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32 -O /opt/
-wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64 -O /opt/
+git clone https://github.com/danielmiessler/SecLists.git /opt/SecLists
+git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
+git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/privilege-escalation-awesome-scripts-suite
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32 -O /opt/pspy32
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64 -O /opt/pspy64
 
 echo "[*] Stuff installed"
