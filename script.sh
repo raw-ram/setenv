bbfcc7961a68b19aad36dbd14eb61ed9f172508a
r4wr4m <61500192+r4wr4m@users.noreply.github.com>
2020-12-09 19:25:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -31,5 +31,6 @@ git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
 git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git /opt/privilege-escalation-awesome-scripts-suite
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32 -O /opt/pspy32
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64 -O /opt/pspy64
+git clone https://github.com/Tib3rius/AutoRecon.git
 
 echo "[*] Stuff installed"
